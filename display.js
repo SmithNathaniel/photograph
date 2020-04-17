c0afd150cc75a5a6aabda1eb5cd872731e3d49a4
SmithNathaniel <37573057+SmithNathaniel@users.noreply.github.com>
2020-04-18 00:50:42 +0200

    Add files via upload

---
+++ display.js
@@ -0,0 +1,14 @@
+function display() {
+  if (document.getElementById('r1').checked) {
+    document.body.style.background = "url(light.jpg) no repeat right top";
+  }
+  else if (document.getElementById('r2').checked) {
+    document.body.style.background = "url(lens.jpg) no repeat right top";
+  }
+  else if (document.getElementById('r2').checked) {
+    document.body.style.background = "url(creative.jpg) no repeat right top";
+  }
+  else if (document.getElementById('r2').checked) {
+    document.body.style.background = "url(light.jpg) no repeat right top";
+  }
+}
